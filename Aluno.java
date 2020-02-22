4216c83d10adad93e269bf42ef9b3742f271a456
Sergio Bonato <asbonato@gmail.com>
2020-02-21 21:50:33 -0300

    versao inicial

---
+++ Aluno.java
@@ -0,0 +1,18 @@
+public class Aluno{
+   private String nome;
+   private int id;
+   
+   public Aluno(String nome, int id){
+      this.nome = nome;
+      this.id = id;
+   }
+   
+   public String getNome(){
+      return this.nome;
+   }
+   
+   public int getId(){
+      return this.id;
+   }
+}
+   
